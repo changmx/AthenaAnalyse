655452ee608307af4433d5beea103e916bdb190d
xuan <qcwz1995@163.com>
2021-03-29 16:20:10 +0800

    feat：在该文件中创建一个类保存常用物理常数

---
+++ CommonCalculation/physicalConstant.py
@@ -0,0 +1,11 @@
+
+
+class const:
+    'fundamental constants of physics'
+    
+    LIGHT_SPEED = 299792458
+
+    MASS_PROTON_EV = 938.27208816e6
+    MASS_PROTON_KG = 1.67262192369e-27
+    MASS_ELECTRON_EV = 0.510998950e6
+    MASS_ELECTRON_KG = 9.1093837015e-31
