e49cae41140004dc04defb3c43d72d8d11e2bce3
xuan <qcwz1995@163.com>
2021-03-29 17:50:49 +0800

    feat：导入相应模块

--- CommonCalculation/physicalConstant.py
+++ CommonCalculation/physicalConstant.py
@@ -1,10 +1,10 @@
-
-
 class const:
     'fundamental constants of physics'
     
     LIGHT_SPEED = 299792458
 
+    ELEMENTARY_CHARGE = 1.602176634e-19
+    
     MASS_PROTON_EV = 938.27208816e6
     MASS_PROTON_KG = 1.67262192369e-27
     MASS_ELECTRON_EV = 0.510998950e6
