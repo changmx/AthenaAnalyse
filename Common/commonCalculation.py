bf0d15ba4defbc562e10c1e1738be1b1af4e114d
xuan <qcwz1995@163.com>
2021-03-30 11:23:30 +0800

    feat：添加能量到速度的计算，添加束团尺寸计算，输入参数能量由总能量改为动能

--- Common/commonCalculation.py
+++ Common/commonCalculation.py
@@ -2,23 +2,36 @@ import numpy as np
 from physicalConstant import const
 
 
-def energy2betagamma(total_energy_ev, invariant_mass_ev):
+def energy2betagamma(kinetic_energy_ev, invariant_mass_ev):
+    "Calculate the Lorentz factor from the kinetic energy"
+    total_energy_ev = kinetic_energy_ev + invariant_mass_ev
+    gamma = total_energy_ev / invariant_mass_ev
+    beta = np.sqrt(1 - 1 / (gamma * gamma))
 
-    # total_energy_ev = kinetic_energy_ev + invariant_mass_ev
-    gamma = total_energy_ev/invariant_mass_ev
-    beta = np.sqrt(1-1/(gamma*gamma))
-    
     return beta, gamma
 
-def intensity(total_energy_ev,invariant_mass_ev,circumference,npPerBunch,nBunch):
-	
-    beta,_ = energy2betagamma(total_energy_ev,invariant_mass_ev)
-    T = circumference/(beta*const.LIGHT_SPEED)
-    I = npPerBunch*nBunch*const.ELEMENTARY_CHARGE/T
+
+def energy2velocity(kinetic_energy_ev, invariant_mass_ev):
+    "Calculate the velocity from the kinetic energy"
+    beta, _ = energy2betagamma(kinetic_energy_ev, invariant_mass_ev)
+
+    return beta * const.LIGHT_SPEED
+
+
+def intensity(kinetic_energy_ev, invariant_mass_ev, circumference, npPerBunch, nBunch):
+    "Calculate beam intensity"
+    T = circumference / energy2velocity(kinetic_energy_ev, invariant_mass_ev)
+    I = npPerBunch * nBunch * const.ELEMENTARY_CHARGE / T
 
     return I
+
+
+def sigma(beta, emit):
+    "Calculate beam size from twiss beta and emittence"
+
+    return np.sqrt(beta * emit)
+
 
 if __name__ == "__main__":
 
     pass
-    
