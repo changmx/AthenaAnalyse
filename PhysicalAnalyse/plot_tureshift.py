15a9f0a29bb913bb1d6a45f88b69c9086d0b3779
xuan <qcwz1995@163.com>
2021-04-02 15:32:56 +0800

    style：使用新定义的函数计算

--- PhysicalAnalyse/plot_tureshift.py
+++ PhysicalAnalyse/plot_tureshift.py
@@ -1,10 +1,10 @@
-from tuneShift import cal_tuneShift
+import Common as co
 
 # EicC
 beta_ex = 0.2
 beta_ey = 0.06
 beta_px = 0.04
-beta_py= 0.02
+beta_py = 0.02
 
 emit_ex = 60e-9
 emit_ey = 60e-9
@@ -14,18 +14,20 @@ emit_py = 180e-9
 sigma_ez = 0.02
 sigma_pz = 0.04
 
-phi = 0*25e-3    # 2*phi is real angles
-
 Ek_e = 3.5e9
 Ek_p = 19.08e9
 
 Ne = 1.7e11
 Np = 1.25e11
-cf = 100e6
 
-xi_e_x = cal_tuneShift(Np,beta_ex,beta_ey,Ek_e,beta_px,beta_py,emit_px,emit_py,"x","electron")
-xi_e_y = cal_tuneShift(Np,beta_ex,beta_ey,Ek_e,beta_px,beta_py,emit_px,emit_py,"y","electron")
-xi_p_x = cal_tuneShift(Ne,beta_px,beta_py,Ek_p,beta_ex,beta_ey,emit_ex,emit_ey,"x","proton")
-xi_p_y = cal_tuneShift(Ne,beta_px,beta_py,Ek_p,beta_ex,beta_ey,emit_ex,emit_ey,"y","proton")
+sigma_ex = co.sigma(beta_ex, emit_ex)
+sigma_ey = co.sigma(beta_ey, emit_ey)
+sigma_px = co.sigma(beta_px, emit_px)
+sigma_py = co.sigma(beta_py, emit_py)
 
-print("xi_e_x:%f, xi_e_y:%f, xi_p_x:%f, xi_p_y:%f"%(xi_e_x,xi_e_y,xi_p_x,xi_p_y))
+xi_e_x, xi_e_y = co.cal_tuneShift(Np, beta_ex, beta_ey, Ek_e, sigma_px,
+                                  sigma_py, co.Const.MASS_ELECTRON_EV, co.Const.RADIUS_ELECTRON)
+xi_p_x, xi_p_y = co.cal_tuneShift(
+    Ne, beta_px, beta_py, Ek_p, sigma_ex, sigma_ey, co.Const.MASS_PROTON_EV, co.Const.RADIUS_PROTON)
+
+print("xi_e_x:%f, xi_e_y:%f\nxi_p_x:%f, xi_p_y:%f"%(xi_e_x,xi_e_y,xi_p_x,xi_p_y))
