0411beffed3f202feabaa63c1400e9a5e77c53ec
xuan <qcwz1995@163.com>
2021-05-06 18:12:46 +0800

    docs：增加KEKB参数

--- PhysicalAnalyse/plot_hourglassSigma.py
+++ PhysicalAnalyse/plot_hourglassSigma.py
@@ -1,27 +1,37 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
-fig,ax = plt.subplots()
+fig, ax = plt.subplots()
 
 sigmaxS = []
 sigmayS = []
 s = []
-# emitx = 3e-7
-# emity = 18e-9
+
+name = 'EicC electron'
 betax = 0.2
 betay = 0.06
-# sigmax = np.sqrt(betax*emitx)
-# sigmay = np.sqrt(betay*emity)
 sigmaZ = 0.02
 
-for position in np.linspace(-6*sigmaZ,6*sigmaZ,100):
-	sigmaxS.append(np.sqrt(1+position**2/(betax**2)))
-	sigmayS.append(np.sqrt(1+position**2/(betay**2)))
-	s.append(position/sigmaZ)
+name = 'EicC proton'
+betax = 0.04
+betay = 0.02
+sigmaZ = 0.04
 
-ax.plot(s,sigmaxS,label=r'$\sigma_x$')
-ax.plot(s,sigmayS,label=r'$\sigma_y$')
+name = 'KEKB'
+betax = 0.33
+betay = 0.01
+sigmaZ = 0.004
+
+for position in np.linspace(-6 * sigmaZ, 6 * sigmaZ, 100):
+    sigmaxS.append(np.sqrt(1 + position**2 / (betax**2)))
+    sigmayS.append(np.sqrt(1 + position**2 / (betay**2)))
+    s.append(position / sigmaZ)
+
+ax.plot(s, sigmaxS, label=r'$\sigma_x$')
+ax.plot(s, sigmayS, label=r'$\sigma_y$')
 ax.set_xlabel(r'$z/\sigma_z$')
+ax.set_ylabel(r'$\sigma/\sigma_0$')
+ax.set_title(name)
 ax.grid()
 ax.legend()
 
