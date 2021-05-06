3109bb0f018b947665727da47423384159f37360
xuan <qcwz1995@163.com>
2021-05-06 18:11:16 +0800

    style：参数修改

--- PhysicalAnalyse/cal_gearChange.py
+++ PhysicalAnalyse/cal_gearChange.py
@@ -1,8 +1,8 @@
 import Common as co
 import numpy as np
 
-v_250 = co.energy2velocity(250e9 - co.const.MASS_NUCLEON_EV, co.const.MASS_NUCLEON_EV)
-v_25 = co.energy2velocity(35.1e9 - co.const.MASS_NUCLEON_EV, co.const.MASS_NUCLEON_EV)
+v_250 = co.energy2velocity(250e9 - co.Const.MASS_NUCLEON_EV, co.Const.MASS_NUCLEON_EV)
+v_25 = co.energy2velocity(25e9 - co.Const.MASS_NUCLEON_EV, co.Const.MASS_NUCLEON_EV)
 print("v250/v25 = %f" % (v_250 / v_25))
 
 L = 3000
@@ -12,13 +12,13 @@ print("L 25Gev = %f" % L_25)
 
 H_250 = 2500
 f_250 = v_250 / L * H_250
-v_35_1 = co.energy2velocity(35.1e9 - co.const.MASS_NUCLEON_EV, co.const.MASS_NUCLEON_EV)
+v_35_1 = co.energy2velocity(35.1e9 - co.Const.MASS_NUCLEON_EV, co.Const.MASS_NUCLEON_EV)
 H_35_1 = f_250 / (v_35_1 / L)
 print(H_35_1)
 
 H_20_4 = 2501
 v_20_4 = f_250 / H_20_4 * L
-beta_20_4 = v_20_4 / co.const.LIGHT_SPEED
+beta_20_4 = v_20_4 / co.Const.LIGHT_SPEED
 gamma_20_4 = 1 / np.sqrt(1 - beta_20_4 ** 2)
-e_20_4 = gamma_20_4 * co.const.MASS_NUCLEON_EV
+e_20_4 = gamma_20_4 * co.Const.MASS_NUCLEON_EV
 print(e_20_4 / 1e9)
