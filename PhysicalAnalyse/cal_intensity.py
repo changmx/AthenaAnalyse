10f0ce77f0a488a7c005209f5081e2a3ada84979
xuan <qcwz1995@163.com>
2021-05-06 18:11:51 +0800

    refactor：使用Ring类优化计算

--- PhysicalAnalyse/cal_intensity.py
+++ PhysicalAnalyse/cal_intensity.py
@@ -1,23 +1,25 @@
 import math
-import sys
-sys.path.append('D:\\AthenaAnalyse\\CommonCalculation')
-import common
-from physicalConstant import const
+import Common as co
 
 # KEKB parameter
-E = 8e9
-circumference = 3016.26
-npPerBunch = 1.4e10
-nBunch = 5000
+# E = 8e9
+# circumference = 3016.26
+# npPerBunch = 1.4e10
+# nBunch = 5000
 
 # eRHIC parameter
-# Ek = 10e9
-# circumference = 3833.845
-# npPerBunch = 3.05e11
-# nBunch = 330
+Ek_p_eRHIC = 275e9
+Ek_e_eRHIC = 18e9
+circumference_p_eRHIC = 3833.94
+circumference_e_eRHIC = 3833.94
+Np_p_eRHIC = 6.9e10
+Np_e_eRHIC = 17.2e10
+nBunch_p_eRHIC = 1160
+nBunch_e_eRHIC = 1160
 
-# I = cal_intensity(Ek,circumference,npPerBunch,nBunch,'electron')
-# print("Beam current: %f A" % (I))Ek = 10e9
+eRHIC = co.Ring(Ek_p_eRHIC, Ek_e_eRHIC, Np_p_eRHIC, Np_e_eRHIC, nBunch_p_eRHIC,
+                nBunch_e_eRHIC, circumference_p_eRHIC, circumference_e_eRHIC,
+                co.Const.MASS_PROTON_EV, co.Const.MASS_ELECTRON_EV)
 
 # BEPC2 parameter
 # Ek = 1.89e9
@@ -25,5 +27,4 @@ nBunch = 5000
 # npPerBunch = 4.85e10
 # nBunch = 1
 
-I = common.intensity(E,const.MASS_ELECTRON_EV,circumference,npPerBunch,nBunch)
-print("Beam current: %f A" % (I))
+eRHIC.print()
