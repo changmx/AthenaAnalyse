f9114545de5346dd9aa29a4899f072c7abe20ba8
xuan <qcwz1995@163.com>
2021-05-24 16:41:33 +0800

    refactor：把绘制理论与模拟沙漏因子的方法用函数表示出来

--- DataAnalyse/沙漏效应理论模拟对比图.py
+++ DataAnalyse/沙漏效应理论模拟对比图.py
@@ -1,39 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sigma_z,r_theory,r_sim_2slice,r_sim_5slice,r_sim_10slice, r_sim_20slice = np.loadtxt(r'D:\AthenaOrigin\hourglass_compare.csv', delimiter=',', skiprows=1, usecols=(0, 1, 2, 3, 4, 5), unpack=True)
-sigma_z=sigma_z*100    # 把单位由m转化为cm
 
-fig,ax = plt.subplots()
-# fig,ax = plt.subplots(figsize=(12,8),dpi=600)
+def plot_hourglass_factor(ax,
+                          sigma,
+                          theory,
+                          sim,
+                          title,
+                          sim_label='Simulated result',
+                          theory_label='Theoretical value'):
+    sigma = sigma * 100
+    ax.scatter(sigma, sim, label=sim_label)
+    ax.plot(sigma, theory, label=theory_label, color='orange')
 
-ax.scatter(sigma_z,r_sim_10slice,label='Simulated result')
-ax.plot(sigma_z,r_theory,color='orange',label='Theoretical value')
-part=0.6
-# 计算相对误差
-error = [0]*sigma_z.shape[0]
-sum_error = 0
-sum_error_first_half = 0
-sum_error_second_half = 0
-for i in range(sigma_z.shape[0]):
-	error[i]=abs(r_sim_10slice[i]-r_theory[i])/r_theory[i]
-	sum_error+=error[i]
-	if i<sigma_z.shape[0]*part:
-		sum_error_first_half+=error[i]
-	else:
-		sum_error_second_half+=error[i]
+    error = [0] * sigma.shape[0]
+    sum_error = 0
 
-ax.text(sigma_z[0],0.5*r_sim_10slice[0],r'$Average\ relative\ error = $'+str(format(sum_error/(sigma_z.shape[0])*100,".2f"))+'%',bbox=dict(boxstyle="round", fc="w"),fontsize='medium')
-# ax.text(sigma_z[0],0.5*r_sim[0],r'$Average\ relative\ error(\sigma_z <3cm)= $'+str(format(sum_error_first_half/(part*sigma_z.shape[0])*100,".2f"))+'%'+'\n'+\
-# 	r'$Average\ relative\ error(\sigma_z  \geq 3cm)= $'+str(format(sum_error_second_half/((1-part)*sigma_z.shape[0])*100,".2f"))+'%',bbox=dict(boxstyle="round", fc="w"),fontsize='medium')
-# ax.text(sigma_z[0],0.46*r_sim[0],'Average relative error of the second half of the data = '+str(format(sum_error_second_half/(sigma_z.shape[0]/2)*100,".2f"))+'%',bbox=dict(boxstyle="round", fc="w"),fontsize='medium')
-ax.locator_params("x",tight=True,nbins=10)
-ax.set_xlabel(r'$\sigma_z\ (cm)$',loc='center')
-ax.set_ylabel('hourglass factor')
-ax.set_ylim(0,1.1)
-ax.set_title('Comparison of simulated and theoretical hourglass factor of gaussian beams (10 slices)')
-ax.grid()
-ax.legend()
+    for i in range(sigma.shape[0]):
+        error[i] = abs(sim[i] - theory[i]) / theory[i]
+        sum_error += error[i]
 
-# plt.savefig(r'./'+'hourglassFactorCompare'+'.png')
-plt.show()
+    ax.text(2,
+            sim[0],
+            r'$Average\ relative\ error = $' +
+            str(format(sum_error / (sigma.shape[0]) * 100, ".2f")) + '%',
+            bbox=dict(boxstyle="round", fc="w"),
+            fontsize='medium')
+
+    ax.locator_params("x", tight=True, nbins=10)
+    ax.set_xlabel(r'$\sigma_z\ (cm)$', loc='center')
+    ax.set_ylabel('hourglass factor')
+    ax.set_ylim(0.6, 1.05)
+    ax.set_title(title)
+    ax.grid()
+    ax.legend()
+
+
+if __name__ == '__main__':
+    sigma_z1_change, theory_z1_change, sim_10slices_z1_change, sigma_z2_change, theory_z2_change, sim_10slices_z2_change, sim_20slices_z2_change, sigma_z1z2_change, theory_z1z2_change, sim_10slices_z1z2_change, sim_20slices_z1z2_change = np.loadtxt(
+        r'D:\研究生\博二\2021_04_hourglass因子对比\hourglass_compare2.csv',
+        delimiter=',',
+        skiprows=1,
+        usecols=(0, 1, 2, 4, 5, 6, 7, 9, 10, 11, 12),
+        unpack=True)
+
+    fig, ax = plt.subplots(1, 3, sharex=True)
+    plot_hourglass_factor(
+        ax[0],
+        sigma_z1_change,
+        theory_z1_change,
+        sim_10slices_z1_change,
+        title=
+        r'Change $\sigma_p$ and keep $\sigma_e = {0}cm$, 10 slices per beam'.
+        format(2))
+
+    plot_hourglass_factor(
+        ax[1],
+        sigma_z2_change,
+        theory_z2_change,
+        sim_10slices_z2_change,
+        title=
+        r'Change $\sigma_e$ and keep $\sigma_p = {0}cm$, 10 slices per beam'.
+        format(4))
+
+    plot_hourglass_factor(
+        ax[2],
+        sigma_z1z2_change,
+        theory_z1z2_change,
+        sim_10slices_z1z2_change,
+        title=r'Change $\sigma_e$ and $\sigma_p$, 10 slices per beam')
+
+    plt.show()
