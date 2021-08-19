de741ab4a26211590eabf105e552ddbe7c03200a
y7000p <qcwz1995@163.com>
2021-08-19 23:08:27 +0800

    feat：参数修改

--- PhysicalAnalyse/cal_synchronize.py
+++ PhysicalAnalyse/cal_synchronize.py
@@ -4,15 +4,17 @@ import numpy as np
 import Common as co
 from cal_centerOfMass_energy import cal_2particle_centerOfMass_energy
 
-Brho = 86
-L = 1341
-quantity = 2
-N_proton = 2
-N_neutron = 1
+Brho = 11.675
+L = 809.44
+# L = 1341.58
+quantity = 1
+N_proton = 0
+N_neutron = 0
+f_colli_input = 100e6
+Nbunch = 270
 
 mass = 0
 N_total = 0
-speed_light = 3e8
 if N_proton == 0 and N_neutron == 0:
     mass = 1 * co.Const.MASS_ELECTRON_EV
     N_total = 1  # 只有一个电子
@@ -23,13 +25,23 @@ else:
     mass = 1 * co.Const.MASS_NUCLEON_EV
     N_total = N_proton + N_neutron
 
-momentum_total = Brho * quantity * speed_light
+momentum_total = Brho * quantity * co.Const.LIGHT_SPEED
 momentum = momentum_total / N_total
+print(20e9/co.Const.LIGHT_SPEED)
+print(3.5e9 / co.Const.LIGHT_SPEED)
 E = np.sqrt(momentum**2 + mass**2)
 Ek = E - mass
 
 v = energy2velocity(Ek, mass)
+f = v / L
+f_colli_cal = f * Nbunch
+L_need = v / (f_colli_input / Nbunch)
 
 print('Kinetic energy(GeV/u): {0:f}'.format(Ek / 1e9))
 print('Momentum(GeV/c/u):     {0:f}'.format(momentum / 1e9))
 print('Total energy(GeV/u):   {0:f}'.format(E / 1e9))
+print('Revolution freq(MHz):  {0:f}'.format(f / 1e6))
+print('Nbunch need:           {0:f}'.format(f_colli_input / f))
+print('L_need:                {0:f}'.format(L_need))
+print('L_need-L:              {0:f}'.format(L_need - L))
+print('f_collision_cal(MHz):  {0:f}'.format(f_colli_cal / 1e6))
