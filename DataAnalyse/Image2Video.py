b4b53231cefc767df509a3b6b49dc9e1f766ddc0
xuan <qcwz1995@163.com>
2021-09-09 17:38:08 +0800

    style：修改参数，绘制粒子分布动画

--- DataAnalyse/Image2Video.py
+++ DataAnalyse/Image2Video.py
@@ -1,18 +1,25 @@
 import cv2
 import os
+from PIL import Image
 
-fps = 10 #视频每秒1帧
-size = (6*300,4*300) #需要转为视频的图片的尺寸,  可以使用cv2.resize()进行修改
-suffix=r'\fig_positron_turn' #文件路径前缀
-angle=11
-path = r'E:\changmx\bb2019\distribution\2020_1103\figure'
-video = cv2.VideoWriter(path+r'\Video_halfPhi_'+str(angle)+'.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, size)   #视频保存在设定目录下, 格式为 motion-jpeg codec，图片颜色失真比较小
-Nturn= 100
-for i in range(1,Nturn+1):
-    png = cv2.imread(path+suffix+str(i)+'.png')
+fps = 20  #视频每秒帧数
+
+path = r'D:\bb2021\statLumiPara\2021_0909\1230_23\figure_distribution\fixPoint\electron_bunch0\1230_23_electron_bunch0_'
+figure_ini_path = path + '0_xpx.png'
+figure_ini_read = Image.open(figure_ini_path)
+figure_w = figure_ini_read.width
+figure_h = figure_ini_read.height
+
+video = cv2.VideoWriter(
+    path + r'\Video_xpx' + '.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+    fps, (figure_w, figure_h))  #视频保存在设定目录下, 格式为 motion-jpeg codec，图片颜色失真比较小
+Nturn = 2048
+for i in range(Nturn):
+    png = cv2.imread(path + str(i) + '_xpx.png')
     # print(path+suffix+str(i)+'.png')
     video.write(png)
-    
+    if i % 100 == 0:
+        print(i)
 
 video.release()
 cv2.destroyAllWindows()
