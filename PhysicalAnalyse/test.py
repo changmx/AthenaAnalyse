0a695c4affa6c0acd76dd6c21b254d02284e386f
xuan <qcwz1995@163.com>
2021-09-11 17:40:41 +0800

    test：测试循环相关内容

--- PhysicalAnalyse/test.py
+++ PhysicalAnalyse/test.py
@@ -1,19 +1,26 @@
 import PyNAFF as pnf
 import numpy as np
 
-N = 512
-t = np.arange(N)
-Q = 0.123456789
-signal = np.sin(2.0 * np.pi * Q * t)
+for i in range(3):
+    print('i', i)
+    for i in range(5):
+        print(i)
+    print(i)
 
-freq_NAFF = pnf.naff(signal, N, 1, 0, False, 1)[0][1]
+# test NAFF
+# N = 512
+# t = np.arange(N)
+# Q = 0.123456789
+# signal = np.sin(2.0 * np.pi * Q * t)
 
-sp = np.fft.fft(signal)
-freq = np.fft.fftfreq(t.shape[-1])
-i = np.argmax(sp)
+# freq_NAFF = pnf.naff(signal, N, 1, 0, False, 1)[0][1]
 
-freq_FFT = freq[i]
+# sp = np.fft.fft(signal)
+# freq = np.fft.fftfreq(t.shape[-1])
+# i = np.argmax(sp)
 
-print(Q)
-print(freq_NAFF)
-print(freq_FFT)
+# freq_FFT = freq[i]
+
+# print(Q)
+# print(freq_NAFF)
+# print(freq_FFT)
