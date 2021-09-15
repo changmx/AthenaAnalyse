e16345a4c9514c4e85ca4fb1910dacbc9e33d0b8
xuan <qcwz1995@163.com>
2021-09-15 16:07:33 +0800

    test：测试

--- PhysicalAnalyse/test.py
+++ PhysicalAnalyse/test.py
@@ -1,11 +1,53 @@
 import PyNAFF as pnf
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-for i in range(3):
-    print('i', i)
-    for i in range(5):
-        print(i)
-    print(i)
+fig, ax = plt.subplots()
+
+
+def f(x, y):
+    return np.sin(x) + np.cos(y)
+
+
+x = np.linspace(0, 2 * np.pi, 120)
+y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
+
+# ims is a list of lists, each row is a list of artists to draw in the
+# current frame; here we are just animating one artist, the image, in
+# each frame
+ims = []
+for i in range(60):
+    x += np.pi / 15.
+    y += np.pi / 20.
+    im = ax.imshow(f(x, y), animated=True)
+    if i == 0:
+        ax.imshow(f(x, y))  # show an initial one first
+    ims.append([im])
+
+ani = animation.ArtistAnimation(fig,
+                                ims,
+                                interval=50,
+                                blit=True,
+                                repeat_delay=1000)
+
+# To save the animation, use e.g.
+#
+ani.save("movie.gif", writer='pillow')
+#
+# or
+#
+# writer = animation.FFMpegWriter(
+#     fps=15, metadata=dict(artist='Me'), bitrate=1800)
+# ani.save("movie.mp4", writer=writer)
+
+plt.show()
+
+# for i in range(3):
+#     print('i', i)
+#     for i in range(5):
+#         print(i)
+#     print(i)
 
 # test NAFF
 # N = 512
