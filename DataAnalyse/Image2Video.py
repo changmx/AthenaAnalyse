813a88201257b5e141c2b3401f050a887418e5bd
xuan <qcwz1995@163.com>
2021-09-15 16:07:17 +0800

    feat：功能优化

--- DataAnalyse/Image2Video.py
+++ DataAnalyse/Image2Video.py
@@ -1,25 +1,31 @@
 import cv2
 import os
 from PIL import Image
+import re
 
-fps = 60  #视频每秒帧数
+fps = 50  #视频每秒帧数
 
-path = r'D:\bb2021\statLumiPara\2021_0909\1230_23\figure_distribution\fixPoint\electron_bunch0_xpx\1230_23_electron_bunch0_xpx_'
-figure_ini_path = path + '0.png'
-figure_ini_read = Image.open(figure_ini_path)
-figure_w = figure_ini_read.width
-figure_h = figure_ini_read.height
+dir = r'D:\bb2021\statLumiPara\2021_0913\1801_59\new1\electron_bunch0_xpx'
+
+name = os.listdir(dir)
+for iname in name:
+    if 'avi' in iname:
+        name.remove(iname)
+name.sort(key=lambda x: int(re.match(r'(.*)_([0-9]*).(.*)', x).group(2)))
+# print(name)
+file = [os.sep.join([dir, iname]) for iname in name]
+# print(file)
+figure_w = Image.open(file[0]).width
+figure_h = Image.open(file[0]).height
 
 video = cv2.VideoWriter(
-    path + r'Video_xpx' + '.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+    os.sep.join([dir, '1.avi']), cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
     fps, (figure_w, figure_h))  #视频保存在设定目录下, 格式为 motion-jpeg codec，图片颜色失真比较小
-Nturn = 2048
-for i in range(Nturn):
-    png = cv2.imread(path + str(i) + '.png')
-    # print(path+suffix+str(i)+'.png')
-    video.write(png)
-    if i % 100 == 0:
-        print(i)
+
+for ifile in (file):
+    print(ifile)
+    figure = cv2.imread(ifile)
+    video.write(figure)
 
 video.release()
 cv2.destroyAllWindows()
