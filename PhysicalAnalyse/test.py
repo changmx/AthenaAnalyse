f055539665e9d33eda3454ede863c544693c65df
xuan <qcwz1995@163.com>
2021-10-11 14:41:21 +0800

    test：测试fft相关内容

--- PhysicalAnalyse/test.py
+++ PhysicalAnalyse/test.py
@@ -3,37 +3,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-fig, ax = plt.subplots()
+    
+N = 1500
+fe = 0.2
+fp = 0.37
+xe = np.arange(N)
+xe1 = np.sin(2*np.pi*fe*xe)
+xe2 = np.sin(2*np.pi*fe*xe)
+
+xp = np.arange(2*N)
+xp1 = 1*np.sin(2*np.pi*fp*xp)
+xp2 = 1*np.sin(2*np.pi*fp*xp)
+xp3 = 1*np.sin(2*np.pi*fp*xp)
+
+for i in range(N):
+    id = int(i/5)
+    if i%5 == 0:
+        xe1[i]+=xp1[id*3+0]
+
+x = xe1
+
+sp = np.fft.fft(x)
+freq = np.fft.fftfreq(x.shape[-1])
+# plt.plot(freq, sp.real, freq, sp.imag)
+plt.plot(freq, np.abs(sp))
+plt.grid()
+plt.show()
+
+# fig, ax = plt.subplots()
 
 
-def f(x, y):
-    return np.sin(x) + np.cos(y)
+# def f(x, y):
+#     return np.sin(x) + np.cos(y)
 
 
-x = np.linspace(0, 2 * np.pi, 120)
-y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
+# x = np.linspace(0, 2 * np.pi, 120)
+# y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
 
-# ims is a list of lists, each row is a list of artists to draw in the
-# current frame; here we are just animating one artist, the image, in
-# each frame
-ims = []
-for i in range(60):
-    x += np.pi / 15.
-    y += np.pi / 20.
-    im = ax.imshow(f(x, y), animated=True)
-    if i == 0:
-        ax.imshow(f(x, y))  # show an initial one first
-    ims.append([im])
+# # ims is a list of lists, each row is a list of artists to draw in the
+# # current frame; here we are just animating one artist, the image, in
+# # each frame
+# ims = []
+# for i in range(60):
+#     x += np.pi / 15.
+#     y += np.pi / 20.
+#     im = ax.imshow(f(x, y), animated=True)
+#     if i == 0:
+#         ax.imshow(f(x, y))  # show an initial one first
+#     ims.append([im])
 
-ani = animation.ArtistAnimation(fig,
-                                ims,
-                                interval=50,
-                                blit=True,
-                                repeat_delay=1000)
+# ani = animation.ArtistAnimation(fig,
+#                                 ims,
+#                                 interval=50,
+#                                 blit=True,
+#                                 repeat_delay=1000)
 
-# To save the animation, use e.g.
-#
-ani.save("movie.gif", writer='pillow')
+# # To save the animation, use e.g.
+# #
+# ani.save("movie.gif", writer='pillow')
 #
 # or
 #
