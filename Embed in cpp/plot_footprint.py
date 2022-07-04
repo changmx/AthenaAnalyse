d3302a9ca393e73e75faded951f97db867edde79
xuan <qcwz1995@163.com>
2022-07-04 14:59:11 +0800

    feat：屏蔽粒子相空间绘制功能

--- Embed in cpp/plot_footprint.py
+++ Embed in cpp/plot_footprint.py
@@ -280,9 +280,9 @@ class FootPrint:
             self.plot_fix_distribution_perturn(fileIndex, para, turn, x, px, y,
                                                py, z, pz, tag, myDistTurnStep,
                                                isDistZip)
-        if plotkind == 'all' or plotkind == 'fma' or plotkind == 'fmafma':
-            self.plot_fma(fileIndex, para, x, y, tag, myfigsize, myfontsize,
-                          myscattersize)
+        # if plotkind == 'all' or plotkind == 'fma' or plotkind == 'fmafma':
+        #     self.plot_fma(fileIndex, para, x, y, tag, myfigsize, myfontsize,
+        #                   myscattersize)
 
         print('File has been drawn: {0}'.format(self.filePath[fileIndex]))
 
